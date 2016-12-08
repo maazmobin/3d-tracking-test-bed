d67940df7d69fe2cac971ae2557e5397ec82d8f0
Zaid Pirwani <email@zaidpirwani.com>
2016-12-08 18:04:40 +0500

    basic code done - NOT writing file on SD

--- _3d_tracking_test_bed/_3d_tracking_test_bed.ino
+++ _3d_tracking_test_bed/_3d_tracking_test_bed.ino
@@ -1,67 +1,107 @@
+#include <SPI.h>
+#include <SD.h>
+const int chipSelect = 10;
+File dataFile;
+
+#include <EEPROM.h>
+int address = 0;
+
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include <Adafruit_GPS.h>
 
+#define GPSECHO  false
+#define LED_PIN 13
+#define gpsSerial Serial3
+
+Adafruit_GPS GPS(&gpsSerial);
 MPU6050 accelgyro;
-
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
-
-#define LED_PIN 13
-
-String imuString = "";
-
-#include <Adafruit_GPS.h>
-
-#define mySerial Serial3
-
-Adafruit_GPS GPS(&mySerial);
-
-#define GPSECHO  false
-
 boolean usingInterrupt = false;
-void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+void useInterrupt(boolean);
 
-String gpsString = "";
-String gpsString1 = "";
+int fileNumber;
+char fileName[] = "3DTrack0000.txt";
 
-void setup()  
-{
-gpsString.reserve(500);
-gpsString1.reserve(200);
+int LED = 12;
 
+unsigned long previousMillis1 = 0;
+const long interval1 = 100;
+unsigned long previousMillis2 = 0;
+const long interval2 = 1000;
+
+String logData = "";
+
+void setup() {
+  logData.reserve(200);
+  
   Serial.begin(115200);
+  fileNumber = EEPROM.read(address) + 1;
+  EEPROM.write(address, fileNumber);
 
+  if(fileNumber>999){
+    fileName[7] = '0'+fileNumber/1000;
+  }
+  if(fileNumber>99){
+    int hund = 0;
+    hund = (fileNumber%1000)/100;
+    fileName[8] = '0'+hund;
+  }
+  if(fileNumber>9){
+    int tens = 0;
+    tens = (fileNumber%100)/10;
+    fileName[9] = '0'+tens;
+  }
+  int units = 0;
+  units = fileNumber%10;
+  fileName[10] = '0'+units;
+  
+  Serial.println("FILE NAME:");
+  Serial.println(fileName);
 
   GPS.begin(9600);
-  mySerial.begin(9600);
-  
+  gpsSerial.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   GPS.sendCommand(PGCMD_ANTENNA);
 
-#ifdef __arm__
-  usingInterrupt = false;  //NOTE - we don't want to use interrupts on the Due
-#else
-  useInterrupt(true);
-#endif
+  #ifdef __arm__
+    usingInterrupt = false;
+  # else
+    useInterrupt(true);
+  #endif
+  
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+  
+  accelgyro.initialize();
 
+  //pinMode(LED, OUTPUT);
+  pinMode(chipSelect,OUTPUT);
+  
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    while(1){
+      digitalWrite(LED, HIGH);
+      delay(100);
+      digitalWrite(LED, LOW);
+      delay(900);
+    }
+    return;
+  }
+  Serial.println("card initialized.");
 
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-
-    accelgyro.initialize();
-
-    // configure Arduino LED for
-    imuString.reserve(200);
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  dataFile = SD.open(fileName, FILE_WRITE);  
 }
 
 #ifdef __AVR__
@@ -69,16 +109,14 @@ gpsString1.reserve(200);
 SIGNAL(TIMER0_COMPA_vect) {
   char c = GPS.read();
   // if you want to debug, this is a good time to do it!
-#ifdef UDR0
-  if (GPSECHO)
-    if (c) UDR0 = c;  
-
-#endif
+  #ifdef UDR0
+    if (GPSECHO)
+      if (c) UDR0 = c;
+  #endif
 }
 
 void useInterrupt(boolean v) {
   if (v) {
-
     OCR0A = 0xAF;
     TIMSK0 |= _BV(OCIE0A);
     usingInterrupt = true;
@@ -90,40 +128,44 @@ void useInterrupt(boolean v) {
 }
 #endif //#ifdef__AVR__
 
-uint32_t timer = millis();
-void loop()                     // run over and over again
-{
-  if (! usingInterrupt) {
-    // read data from the GPS in the 'main loop'
-    char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    if (GPSECHO)
-      if (c) Serial.print(c);
-  }
 
-  if (GPS.newNMEAreceived()) {
+void loop(){
   
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
+  if (!usingInterrupt) {
+    char c = GPS.read();
+    if (GPSECHO)
+          if (c) Serial.print(c);
+  }
+  if (GPS.newNMEAreceived()) {
+    Serial.println(GPS.lastNMEA());
+    if (!GPS.parse(GPS.lastNMEA()))
+      return; 
   }
 
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis())  timer = millis();
+  unsigned long currentMillis = millis();
 
-  // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) { 
-    timer = millis(); // reset the timer
-    gpsString="\nTime: "+ String(GPS.hour, DEC)+':'+String(GPS.minute, DEC)+':'+String(GPS.seconds, DEC)+'.'+String(GPS.milliseconds)+"\nDate: "+String(GPS.day, DEC)+'/'+String(GPS.month, DEC)+"/20"+String(GPS.year, DEC)+"\nFix: "+String((int)GPS.fix)+" quality: "+String(((int)GPS.fixquality));
+  if (currentMillis - previousMillis1 >= interval1) {
+    previousMillis1 = currentMillis;
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    logData = "IMU,"+String(ax)+","+String(ay)+","+String(az)+","+String(gx)+","+String(gy)+","+String(gz)+"\n";
+    Serial.println(logData);
+    dataFile.print(logData);
+    dataFile.flush();
+  }
 
+  if (currentMillis - previousMillis2 >= interval2) {
+    previousMillis2 = currentMillis;
+    logData = "Time: "+ String(GPS.hour, DEC)+':'+String(GPS.minute, DEC)+':'+String(GPS.seconds, DEC)+'.'+String(GPS.milliseconds)+"Date: "+String(GPS.day, DEC)+'/'+String(GPS.month, DEC)+"/20"+String(GPS.year, DEC)+"Fix: "+String((int)GPS.fix)+" quality: "+String(((int)GPS.fixquality))+"\n";
+    Serial.print(logData);
+    dataFile.print(logData);
     if (GPS.fix) {
-      gpsString1="\nLocation: "+String(GPS.latitude, 2)+String(GPS.lat)+", "+String(GPS.longitude, 2)+String(GPS.lon)+"\nSpeed (knots): "+String(GPS.speed)+"\nAngle: "+String(GPS.angle)+"\nAltitude: "+String(GPS.altitude)+"\nSatellites: "+String((int)GPS.satellites);
-      gpsString=gpsString+gpsString1;
-
-    }    
-    Serial.println(gpsString);
-        accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-imuString="\nIMU,"+String(ax)+","+String(ay)+","+String(az)+","+String(gx)+","+String(gy)+","+String(gz);
-        // display tab-separated accel/gyro x/y/z values
-        Serial.println(imuString);  
+      pinMode(LED,HIGH);
+      logData = "Location: "+String(GPS.latitude, 2)+String(GPS.lat)+", "+String(GPS.longitude, 2)+String(GPS.lon)+"\nSpeed (knots): "+String(GPS.speed)+"Angle: "+String(GPS.angle)+"Altitude: "+String(GPS.altitude)+"Satellites: "+String((int)GPS.satellites)+"\n";
+      Serial.print(logData);
+      dataFile.print(logData);
+    }else{
+      pinMode(LED,LOW);
+    }
+    dataFile.flush();
   }
 }
