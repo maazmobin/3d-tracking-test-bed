4bf37712ffb0957553cafd6c96c2e645606b13f8
Zaid Pirwani <email@zaidpirwani.com>
2017-01-24 15:27:16 +0500

    code cleanup and fixing - Works with MUSAFIR
    also, PULL REQUEST

--- _3d_tracking_test_bed/_3d_tracking_test_bed.ino
+++ _3d_tracking_test_bed/_3d_tracking_test_bed.ino
@@ -1,11 +1,3 @@
-#include <SPI.h>
-#include <SD.h>
-const int chipSelect = 10;
-File dataFile;
-
-#include <EEPROM.h>
-int address = 0;
-
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
@@ -15,6 +7,7 @@ int address = 0;
 
 #define GPSECHO  false
 #define LED_PIN 13
+
 #define gpsSerial Serial3
 
 Adafruit_GPS GPS(&gpsSerial);
@@ -24,51 +17,135 @@ int16_t gx, gy, gz;
 
 boolean usingInterrupt = false;
 void useInterrupt(boolean);
-
-int fileNumber;
-char fileName[] = "3DTrack0000.txt";
-
 int LED = 12;
+int BTN = 11;
 
-unsigned long previousMillis1 = 0;
-const long interval1 = 100;
-unsigned long previousMillis2 = 0;
-const long interval2 = 1000;
+#include <SPI.h>
+#include "SdFat.h"
 
-String logData = "";
+String inputString2 = "";         // a string to hold incoming data
+boolean stringComplete2 = false;  // whether the string is complete
 
+
+
+unsigned long previousMillis = 0;
+unsigned long previousMillisGPS = 0;
+
+const uint8_t chipSelect = 10;
+
+const uint32_t SAMPLE_INTERVAL_MS = 20;
+const uint32_t SAMPLE_INTERVAL_MS_GPS = 1000;
+
+// Log file base name.  Must be six characters or less.
+#define FILE_BASE_NAME "Data"
+//------------------------------------------------------------------------------
+// File system object.
+SdFat sd;
+
+// Log file.
+SdFile file;
+
+// Time in micros for next data record.
+uint32_t logTime;
+
+// Write data header.
+void writeHeader() {
+  file.print(F("millis,"));
+  file.print(F("ax,ay,az,gx,gy,gz,"));
+  file.print(F("hour,minute,second,day,month,year,"));
+  file.print(F("fix,quality,"));
+  file.print(F("lat,long,speed,angle,altitude,satellite"));
+  //  for (uint8_t i = 0; i < ANALOG_COUNT; i++) {
+  //    file.print(F(",adc"));
+  //    file.print(i, DEC);
+  //  }
+  file.println();
+}
+//------------------------------------------------------------------------------
+// Log a data record.
+void logData(int gps) {
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  // Write data to file.  Start with log time in micros.
+  file.print(millis());
+  file.write(',');
+  file.print(ax);
+  file.write(',');
+  file.print(ay);
+  file.write(',');
+  file.print(az);
+  file.write(',');
+  file.print(gx);
+  file.write(',');
+  file.print(gy);
+  file.write(',');
+  file.print(gz);
+  file.write(',');
+  int odo=0;
+  if (stringComplete2) {
+    file.print(inputString2);
+    // clear the string:
+    inputString2 = "";
+    stringComplete2 = false;
+    odo=1;
+  }
+  if(gps==1){
+    if(odo==0){
+      file.print(",,,,,");
+    }
+    file.write(',');
+    file.print(GPS.hour);
+    file.write(',');
+    file.print(GPS.minute);
+    file.write(',');
+    file.print(GPS.seconds);
+    file.write(',');
+    file.print(GPS.day);
+    file.write(',');
+    file.print(GPS.month);
+    file.write(',');
+    file.print(GPS.year);
+    file.write(',');
+    file.print((int)GPS.fix);
+    file.write(',');
+    file.print((int)GPS.fixquality);
+    file.write(',');
+//    Serial.print("LAT: ");
+//    Serial.print(GPS.latitude, 8);
+//    Serial.println(GPS.lat);
+    file.print(GPS.latitude, 8);
+    file.print(GPS.lat);
+    file.write(',');
+    file.print(GPS.longitude, 8);
+    file.print(GPS.lon);
+    file.write(',');
+    file.print(GPS.speed, 4);
+    file.write(',');
+    file.print(GPS.angle, 4);
+    file.write(',');
+    file.print(GPS.altitude, 4);
+    file.write(',');
+    file.print(GPS.satellites);
+  }
+  file.println();
+}
+//==============================================================================
+// Error messages stored in flash.
+#define error(msg) sd.errorHalt(F(msg))
+//------------------------------------------------------------------------------
 void setup() {
-  logData.reserve(200);
-  
-  Serial.begin(115200);
-  fileNumber = EEPROM.read(address) + 1;
-  EEPROM.write(address, fileNumber);
+  Serial2.begin(115200);
+  inputString2.reserve(40);
 
-  if(fileNumber>999){
-    fileName[7] = '0'+fileNumber/1000;
-  }
-  if(fileNumber>99){
-    int hund = 0;
-    hund = (fileNumber%1000)/100;
-    fileName[8] = '0'+hund;
-  }
-  if(fileNumber>9){
-    int tens = 0;
-    tens = (fileNumber%100)/10;
-    fileName[9] = '0'+tens;
-  }
-  int units = 0;
-  units = fileNumber%10;
-  fileName[10] = '0'+units;
+  pinMode(LED, OUTPUT);
+  pinMode(BTN, INPUT);
+  digitalWrite(BTN, HIGH); // pulled Up
   
-  Serial.println("FILE NAME:");
-  Serial.println(fileName);
-
   GPS.begin(9600);
   gpsSerial.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   GPS.sendCommand(PGCMD_ANTENNA);
+  delay(2000);
 
   #ifdef __arm__
     usingInterrupt = false;
@@ -83,25 +160,72 @@ void setup() {
   #endif
   
   accelgyro.initialize();
-
-  //pinMode(LED, OUTPUT);
-  pinMode(chipSelect,OUTPUT);
   
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    while(1){
-      digitalWrite(LED, HIGH);
-      delay(100);
-      digitalWrite(LED, LOW);
-      delay(900);
-    }
-    return;
-  }
-  Serial.println("card initialized.");
+  const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+  char fileName[13] = FILE_BASE_NAME "00.csv";
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  dataFile = SD.open(fileName, FILE_WRITE);  
+  Serial.begin(115200);
+  
+  // Wait for USB Serial 
+  while (!Serial) {
+    SysCall::yield();
+  }
+  delay(1000);
+
+  Serial.println(F("Type any character to start"));
+  while (!Serial.available() && digitalRead(BTN)) {
+    SysCall::yield();
+    digitalWrite(LED, HIGH);
+    delay(50);
+    digitalWrite(LED, LOW);
+    delay(1000);
+  }
+
+  while(digitalRead(BTN) == LOW){
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);    
+    delay(1000);
+  }
+  
+  // Initialize the SD card at SPI_HALF_SPEED to avoid bus errors with
+  // breadboards.  use SPI_FULL_SPEED for better performance.
+  if (!sd.begin(chipSelect, SPI_HALF_SPEED)) {
+    sd.initErrorHalt();
+  }
+
+  // Find an unused file name.
+  if (BASE_NAME_SIZE > 6) {
+    error("FILE_BASE_NAME too long");
+  }
+  while (sd.exists(fileName)) {
+    if (fileName[BASE_NAME_SIZE + 1] != '9') {
+      fileName[BASE_NAME_SIZE + 1]++;
+    } else if (fileName[BASE_NAME_SIZE] != '9') {
+      fileName[BASE_NAME_SIZE + 1] = '0';
+      fileName[BASE_NAME_SIZE]++;
+    } else {
+      error("Can't create file name");
+    }
+  }
+  if (!file.open(fileName, O_CREAT | O_WRITE | O_EXCL)) {
+    error("file.open");
+  }
+  // Read any Serial data.
+  do {
+    delay(10);
+  } while (Serial.available() && Serial.read() >= 0);
+
+  Serial.print(F("Logging to: "));
+  Serial.println(fileName);
+  Serial.println(F("Type any character to stop"));
+
+  // Write data header.
+  writeHeader();
+
+  // Start on a multiple of the sample interval.
+  logTime = micros()/(1000UL*SAMPLE_INTERVAL_MS) + 1;
+  logTime *= 1000UL*SAMPLE_INTERVAL_MS;
 }
 
 #ifdef __AVR__
@@ -129,43 +253,71 @@ void useInterrupt(boolean v) {
 #endif //#ifdef__AVR__
 
 
-void loop(){
-  
+
+
+
+
+void loop() {
   if (!usingInterrupt) {
     char c = GPS.read();
     if (GPSECHO)
           if (c) Serial.print(c);
   }
   if (GPS.newNMEAreceived()) {
-    Serial.println(GPS.lastNMEA());
+  //    Serial.println(GPS.lastNMEA());
     if (!GPS.parse(GPS.lastNMEA()))
       return; 
   }
 
+  
   unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis1 >= interval1) {
-    previousMillis1 = currentMillis;
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    logData = "IMU,"+String(ax)+","+String(ay)+","+String(az)+","+String(gx)+","+String(gy)+","+String(gz)+"\n";
-    Serial.println(logData);
-    dataFile.print(logData);
-    dataFile.flush();
-  }
-
-  if (currentMillis - previousMillis2 >= interval2) {
-    previousMillis2 = currentMillis;
-    logData = "Time: "+ String(GPS.hour, DEC)+':'+String(GPS.minute, DEC)+':'+String(GPS.seconds, DEC)+'.'+String(GPS.milliseconds)+"Date: "+String(GPS.day, DEC)+'/'+String(GPS.month, DEC)+"/20"+String(GPS.year, DEC)+"Fix: "+String((int)GPS.fix)+" quality: "+String(((int)GPS.fixquality))+"\n";
-    Serial.print(logData);
-    dataFile.print(logData);
-    if (GPS.fix) {
-      pinMode(LED,HIGH);
-      logData = "Location: "+String(GPS.latitude, 2)+String(GPS.lat)+", "+String(GPS.longitude, 2)+String(GPS.lon)+"\nSpeed (knots): "+String(GPS.speed)+"Angle: "+String(GPS.angle)+"Altitude: "+String(GPS.altitude)+"Satellites: "+String((int)GPS.satellites)+"\n";
-      Serial.print(logData);
-      dataFile.print(logData);
-    }else{
-      pinMode(LED,LOW);
+  if (currentMillis - previousMillis >= SAMPLE_INTERVAL_MS) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    int gps = 0;
+    if(currentMillis - previousMillisGPS >= SAMPLE_INTERVAL_MS_GPS){
+      previousMillisGPS = currentMillis;
+      gps=1;
     }
-    dataFile.flush();
+    else{
+      gps=0;
+    }
+    logData(gps);
+
+    // Force data to SD and update the directory entry to avoid data loss.
+    if (!file.sync() || file.getWriteError()) {
+      error("write error");
+    }
+  }
+  
+  if (digitalRead(BTN)==LOW) {
+    delay(500);
+    if(digitalRead(BTN)==LOW){
+      // Close file and stop.
+      file.close();
+      Serial.println(F("Done"));
+      while(digitalRead(BTN) == LOW){
+        digitalWrite(LED, HIGH);
+        delay(500);
+        digitalWrite(LED, LOW);    
+        delay(500);
+      }
+      SysCall::halt();
+    }
+  }
+}
+
+void serialEvent2(){
+  while (Serial2.available()) {
+    // get the new byte:
+    char inChar = (char)Serial2.read();
+    // add it to the inputString:
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete2 = true;
+    }else{
+      inputString2 += inChar;
+    }
   }
 }
